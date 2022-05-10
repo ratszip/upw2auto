9c9fd94e62bd1c46d3b104c13bebe53566e093db
xzp <civilizationfor@outlook.com>
2022-05-10 17:32:36 +0800

    Initial commit

---
+++ src/test/java/com/upwhk/AppTest.java
@@ -0,0 +1,22 @@
+package com.upwhk;
+
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @BeforeClass
+    @Test
+    public void prePare()
+    {
+        Assert.assertTrue(true);
+    }
+}
